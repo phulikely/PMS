2251e27847d4fd94e8be3d5e574d8ac3eaa63e34
Phulikely <phulikely@gmail.com>
2021-05-21 10:06:38 +0700

    from MySQL to SQLite3

--- pms/settings.py
+++ pms/settings.py
@@ -76,23 +76,23 @@ WSGI_APPLICATION = 'pms.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'pms',
-        'USER': 'root',
-        'PASSWORD': 'dmmt',
-        'HOST': '127.0.0.1',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'pms',
+#         'USER': 'root',
+#         'PASSWORD': 'dmmt',
+#         'HOST': '127.0.0.1',
+#         'PORT': '3306',
+#     }
+# }
 
 
 # Password validation
